9f969a4f6f131e2e2a80546570a6e2698080eb69
raidontpaizer <raidotp7@proton.me>
2025-01-24 02:20:10 +0000

    Updated Index

--- public_html/index.php
+++ public_html/index.php
@@ -7,14 +7,81 @@
         <meta name="description" content="tilde.team unix group">
         <meta name="author" content="<?=$user?>">
         <meta name="theme-color" content="#00cc00">
-        <title><?=$user?>~tilde.team</title>
-        <link rel="stylesheet" type="text/css" href="https://tilde.team/css/hacker.css">
-    </head>
-    <body>
-	<div align="center">
-	<h1>UNDER CONSTRUCTION</h1>
-	<a href="https://github.com/raidontpaizer">Check out my Github for progress if ya care ;D</a>
-   	</div> 
-   </body>
-</html>
+        <title><?=$user?></title>
+        <link rel="stylesheet" type="text/css" href="https://tilde.team/css/dracula.css">
+        <script src="https://kit.fontawesome.com/8f89600519.js" crossorigin="anonymous"></script>
+<style>
+.container {
+width: 60%;
+border: solid white 3px;
+margin-left: auto;
+margin-right: auto;
+}
+.titlebox {
+    padding-top: 2px;
+    text-align: left;
+    margin-left: 5%;
+}
+.userimgar{
+    width: 30%;
+    margin-right: auto;
+    margin-left: 5%;
+}
+.userar{
+    width: 80%;
+    margin-right: auto;
+    margin-left: 5%;
+}
+</style> 
+</head>
+<body>
 
+<div class="container">
+
+<div class="titlebox">
+<h1>~raidopaizer</h1>
+</div>
+
+<div class="userimgar">
+    <img src="nano.jpg" width="80%">
+    </div>
+  <div class="userar">  
+    <h3>Haii Haii!! I'm raid!(any pronouns fine) </h3>
+    <h4>I'm a Highschooler who likes marching band, and computers!</h4>
+    <h6>
+        Knowlege: Decent html knowlege, and how to run a webserver on the nintendo wii!!
+    </h6>
+    <h6>
+        Wishes: To learn c++, and to learn how to draw!
+    </h6>
+        
+    <h4>
+        <i class="fa-solid fa-rss"></i><a href="/~raidopaizer/blog">Check out my blog :D</a>
+    </h4>
+    
+    <h5>Here's some of my social links, ones I choose to share:</h5>
+    <h5>
+        <i class="fa-brands fa-github"></i><a href="https://github.com/raidontpaizer">Github</a>
+    </h5>    
+    
+    <h5>
+        <i class="fa-brands fa-discord"></i><a href="https://discordapp.com/users/1242547690760306813">Discord</a>
+    </h5>
+    
+    
+    <h5>    
+        <i class="fa-brands fa-steam"></i><a href="https://steamcommunity.com/id/nandraid">Main Steam</a>
+    </h5>    
+    
+    <h5>    
+        <i class="fa-brands fa-steam"></i><a href="https://steamcommunity.com/id/raid2004lol">Second Steam</a>
+    </h5>
+</div>
+
+
+
+
+
+</div>
+  </body>
+</html>
